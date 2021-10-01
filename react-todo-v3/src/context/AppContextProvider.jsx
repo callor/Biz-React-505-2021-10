d2092eeb59c288a1a5b837579bfeb4eca1388a44
callor <callor@callor.com>
2021-10-01 17:12:21 +0900

    todo

--- react-todo-v3/src/context/AppContextProvider.jsx
+++ react-todo-v3/src/context/AppContextProvider.jsx
@@ -36,11 +36,12 @@ function AppContextProvider({ children }) {
     t_comp: false,
   });
 
-  const [todoList, setTodoList] = useState([todo]);
+  const [todoList, setTodoList] = useState([]);
 
   // Referrence 변수선언하기
 
   const nextId = useRef(0);
+  const inputId = useRef();
 
   const onChange = (e) => {
     const t_text = e.target.value;
@@ -53,6 +54,11 @@ function AppContextProvider({ children }) {
 
   // 리스트에 추가하기
   const todoInsert = () => {
+    if (todo.t_text === "") {
+      window.alert("할일을 입력하세요 ~~~");
+      inputId.current.focus();
+      return;
+    }
     setTodoList([...todoList, todo]);
     nextId.current++;
     console.log(nextId.current);
@@ -78,7 +84,52 @@ function AppContextProvider({ children }) {
     }
   };
 
-  const propsStrore = { todo, todoList, onChange, onClick, onKeyPress };
+  const onDeleteClick = (e) => {
+    if (window.confirm("삭제할까요?")) {
+      // data-todo-id 라고 저장하면 앞에 data-는 dataset으로 변경
+      // todo-id 는 lower Camel case로 변경되어 todoId 변수에서
+      //		getter 한다
+      const t_id = Number(e.target.dataset.todoId);
+      alert(t_id);
+      // 배열요소중에서 t_id 일치하는 요소를 삭제하기
+      // 원래 배열요소를 filtering하는데..
+      // t_id값이 dataset의 todoId와 일치하지 않는 것만
+      // 새로운 배열로 만들어라
+      const _todoList = todoList.filter((todo) => todo.t_id !== t_id);
+      setTodoList(_todoList);
+
+      // alert("삭제됨");
+    }
+  };
+
+  const onCompClick = (e) => {
+    const t_id = Number(e.target.dataset.todoId);
+
+    // 배열요소중에 조건에 맞는 값이 있으면 그 값이 몇번째
+    // 요소인지 index를 return 한다
+    const index = todoList.findIndex((todo) => todo.t_id === t_id);
+    // 찾았으면 ~~~
+    // 해당 요소만 따로 추출하여 selectTodo에 담기
+    const selectTodo = todoList[index];
+
+    const _todoList = [...todoList];
+    _todoList[index] = {
+      ...selectTodo,
+      t_comp: !selectTodo.t_comp,
+    };
+    setTodoList(_todoList);
+  };
+
+  const propsStrore = {
+    todo,
+    todoList,
+    inputId,
+    onChange,
+    onClick,
+    onKeyPress,
+    onDeleteClick,
+    onCompClick,
+  };
 
   return (
     <AppContext.Provider value={propsStrore}>{children}</AppContext.Provider>
