0b0e85b5e51c6688952ea688994e4fee9dd8a5a3
callor <callor@callor.com>
2021-10-13 15:15:04 +0900

    todo

--- react-todo-v5/src/comps/LoginRoute.jsx
+++ react-todo-v5/src/comps/LoginRoute.jsx
@@ -1,7 +1,9 @@
 import { BrowserRouter } from "react-router-dom";
 
 const LoginRoute = ({ children }) => {
-  return <BrowserRouter>{children}</BrowserRouter>;
+  return (
+    <BrowserRouter basename={process.env.PUBLIC_URL}>{children}</BrowserRouter>
+  );
 };
 
 export default LoginRoute;
